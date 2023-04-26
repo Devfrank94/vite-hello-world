64788dc40015e35138bf8c2a58b618366549da4c
Devfrank94 <francescomurro94@gmail.com>
2023-04-26 17:39:02 +0200

    last commit

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,31 @@
   
 <script>
-export default {
+import AppTitle from "./components/AppTitle.vue"
 
+export default {
+  name: "App",
+  components:{
+    AppTitle
+  },
+  data(){
+    return{
+      title:'Vite Hello World'
+    }
+  }
 }
 </script>
 
 
 <template>
-  
+
+  <h1>{{ title }}</h1>
+  <AppTitle/>
+
 </template>
 
 
-<style>
-
+<style scoped>
+  h1{
+    color: green;
+  }
 </style>
---
+++ src/components/AppTitle.vue
@@ -0,0 +1,28 @@
+  
+<script>
+export default {
+  name: "AppTitle",
+  components:{
+    
+  },
+  data(){
+    return{
+      subTitle:'La mia prima app con Vite!'
+    }
+  }
+}
+</script>
+
+
+<template>
+
+  <h2>{{ subTitle }}</h2>
+
+</template>
+
+
+<style scoped>
+  h2{
+    color: red;
+  }
+</style>
